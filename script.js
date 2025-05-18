66eeb12dbc649b1519a9accc58c7f9838eaab077
newaz-akib <newazakib98@gmail.com>
2025-05-18 17:43:10 +0600

    new upload

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ document.getElementById('contactForm').addEventListener('submit', function(e) {
   document.getElementById('form-message').textContent = " Thank you for message.I will reply back to you soon!";
   this.reset();
 });
+
+
+
+
